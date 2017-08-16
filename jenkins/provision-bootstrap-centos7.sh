4108a37dd08b543b62feafbee9317ae1033a3695
Faizan Muhammad <fmud@tobii.com>
2017-08-16 14:45:59 +0200

    Added project, provision jenkins using ansible and vagrant

---
+++ jenkins/provision-bootstrap-centos7.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Install JDK (and not only JRE)
+#sudo yum install -y java-1.8.0-openjdk-devel
+
+# install git
+#sudo yum install -y git
+
+#Install Jenkins
+#sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+#sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+#sudo yum -y install jenkins
+
